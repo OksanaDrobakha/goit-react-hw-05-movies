3e8b572b1ab7c5fbf663c83e00e8f5122eadeddc
OksanaDrobakha <6249784@gmail.com>
2023-06-08 14:14:30 +0400

    test2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ export const App = () => {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 20,
+        fontSize: 50,
         color: '#010101',
       }}
     >
